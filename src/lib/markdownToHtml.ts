3492900f1bfa5de64c2318ca67c173391e283daa
Stefan Todorovic <stefantodorovic@gmail.com>
2024-02-21 09:58:24 -0500

    tried to fix posts ids

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,11 +1,12 @@
 import { remark } from 'remark';
 import html from 'remark-html';
-import slug from 'remark-slug'; // Import remark-slug
+import rehypeSlug from 'rehype-slug'
+
 
 export default async function markdownToHtml(markdown: string) {
   const result = await remark()
-    .use(slug) // Use remark-slug to add IDs to headings
-    .use(html, { sanitize: false }) // This configures remark-html to not sanitize HTML
+  .use(rehypeSlug)
+      .use(html, { sanitize: false }) // This configures remark-html to not sanitize HTML
     .process(markdown);
   return result.toString();
 }
